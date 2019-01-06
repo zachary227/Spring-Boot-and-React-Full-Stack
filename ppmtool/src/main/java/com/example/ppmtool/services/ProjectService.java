f593a6e90c7598563841cda2e6084249ba6fdc07
Zack <zetaozhuang227@126.com>
2019-01-05 21:12:51 -0800

    Message

---
+++ ppmtool/src/main/java/com/example/ppmtool/services/ProjectService.java
@@ -0,0 +1,24 @@
+package com.example.ppmtool.services;
+
+import com.example.ppmtool.domain.Project;
+import com.example.ppmtool.exceptions.ProjectIdException;
+import com.example.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch( Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+    }
+}
+
